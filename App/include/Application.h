7fd2207058b8259bed6b15a978ab027c6c1e7540
Ren Davis <rcdavisgd@gmail.com>
2026-03-24 15:02:15 -0400

    Add fetching of volk, vma, and vulkan (#2)
    * Added fetching of volk, vma, and vulkan
    
    * Init volk

--- App/include/Application.h
+++ App/include/Application.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "volk.h"
+
 class Application {
 public:
 	Application() = default;
---
+++ App/src/Utils/VkUtils.cpp
@@ -0,0 +1,4 @@
+#include "Utils/VkUtils.h"
+
+#define VMA_IMPLEMENTATION
+#include "vk_mem_alloc.h"
