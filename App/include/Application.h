fa1873cda2eac4d7bb95819e49204b6177c3661c
Ren Davis <rcdavisgd@gmail.com>
2026-04-07 03:29:24 -0400

    Add SDL (#3)
    * Have SDL window showing
    
    * Added SDL Renderer
    
    * Added early outs on error

--- App/include/Application.h
+++ App/include/Application.h
@@ -2,7 +2,13 @@
 
 #include "volk.h"
 
+#include "SDL3/SDL.h"
+
 class Application {
+public:
+	constexpr static int WIDTH = 800;
+	constexpr static int HEIGHT = 600;
+
 public:
 	Application() = default;
 	~Application();
@@ -10,6 +16,14 @@ public:
 	void Run();
 
 private:
-	void Init();
+	bool Init();
 	void Shutdown();
+
+	void MainLoop();
+	void Render();
+
+private:
+	SDL_Window* mWindow = nullptr;
+	SDL_Renderer* mRenderer = nullptr;
+	bool mIsRunning = false;
 };
--- App/src/Application.cpp
+++ App/src/Application.cpp
@@ -7,19 +7,72 @@ Application::~Application() {
 }
 
 void Application::Run() {
-	Init();
+	if (!Init()) {
+		return;
+	}
 
 	LOG_INFO("Application is running!");
+	MainLoop();
 
-	Shutdown();
+	//Shutdown();
 }
 
-void Application::Init() {
+bool Application::Init() {
 	if (volkInitialize() != VK_SUCCESS) {
 		LOG_ERROR("Failed to initialize volk!");
+		return false;
 	}
+
+	if (!SDL_Init(SDL_INIT_VIDEO)) {
+		LOG_ERROR("Failed to initialize SDL: {}", SDL_GetError());
+		return false;
+	}
+
+	if (!SDL_CreateWindowAndRenderer("Vulkan Tutorial", WIDTH, HEIGHT, SDL_WINDOW_VULKAN, &mWindow, &mRenderer)) {
+		LOG_ERROR("Failed to create SDL window and/or renderer: {}", SDL_GetError());
+		return false;
+	}
+
+	mIsRunning = true;
+
+	LOG_INFO("Application initialized successfully!");
+	return true;
 }
 
 void Application::Shutdown() {
+	LOG_INFO("Shutting down application...");
 
+	if (mRenderer) {
+		SDL_DestroyRenderer(mRenderer);
+		mRenderer = nullptr;
+	}
+
+	if (mWindow) {
+		SDL_DestroyWindow(mWindow);
+		mWindow = nullptr;
+	}
+
+	SDL_Quit();
+}
+
+void Application::MainLoop() {
+	SDL_Event event;
+	SDL_zero(event);
+
+	while (mIsRunning) {
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_EVENT_QUIT) {
+				mIsRunning = false;
+			}
+		}
+
+		Render();
+	}
+}
+
+void Application::Render() {
+	SDL_SetRenderDrawColor(mRenderer, 255, 0, 255, 255);
+	SDL_RenderClear(mRenderer);
+
+	SDL_RenderPresent(mRenderer);
 }
